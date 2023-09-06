e413691a472e30db3249dd3958ee893d308b38dc
DESKTOP-6RA1I0Q\nhutt <nhuttantranle@gmail.com>
2023-09-06 22:56:10 +0700

    edit login

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
@@ -52,27 +53,35 @@ app.get("/users/:id", (req, res) => {
 
 app.post("/users", (req, res) => {
   const { username, password, email } = req.body;
-  db.run("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", [username, password, email], (err) => {
-    if (err) {
-      console.error("Error creating user:", err);
-      res.status(500).json({ error: "Internal server error" });
-    } else {
-      res.json({ message: "User created successfully" });
+  db.run(
+    "INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+    [username, password, email],
+    (err) => {
+      if (err) {
+        console.error("Error creating user:", err);
+        res.status(500).json({ error: "Internal server error" });
+      } else {
+        res.json({ message: "User created successfully" });
+      }
     }
-  });
+  );
 });
 
 app.put("/users/:id", (req, res) => {
   const { id } = req.params;
   const { username, password, email } = req.body;
-  db.run("UPDATE users SET username = ?, password = ?, email = ? WHERE id = ?", [username, password, email, id], (err) => {
-    if (err) {
-      console.error("Error updating user:", err);
-      res.status(500).json({ error: "Internal server error" });
-    } else {
-      res.json({ message: "User updated successfully" });
+  db.run(
+    "UPDATE users SET username = ?, password = ?, email = ? WHERE id = ?",
+    [username, password, email, id],
+    (err) => {
+      if (err) {
+        console.error("Error updating user:", err);
+        res.status(500).json({ error: "Internal server error" });
+      } else {
+        res.json({ message: "User updated successfully" });
+      }
     }
-  });
+  );
 });
 
 app.delete("/users/:id", (req, res) => {
@@ -83,6 +92,33 @@ app.delete("/users/:id", (req, res) => {
       res.status(500).json({ error: "Internal server error" });
     } else {
       res.json({ message: "User deleted successfully" });
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  db.get("SELECT * FROM users WHERE username = ?", [username], (err, row) => {
+    if (err) {
+      console.error("Error getting user:", err);
+      res.status(500).json({ error: "Internal server error" });
+    } else if (row) {
+      // So sánh mật khẩu đã nhập với mật khẩu đã được mã hóa trong cơ sở dữ liệu
+      bcrypt.compare(password, row.password, (err, result) => {
+        if (err) {
+          console.error("Error comparing passwords:", err);
+          res.status(500).json({ error: "Internal server error" });
+        } else if (result) {
+          // Mật khẩu khớp, đăng nhập thành công
+          res.json({ message: "Đăng nhập thành công" });
+        } else {
+          // Mật khẩu không khớp, đăng nhập thất bại
+          res.status(401).json({ error: "Mật khẩu không chính xác" });
+        }
+      });
+    } else {
+      // Người dùng không tồn tại
+      res.status(404).json({ error: "Người dùng không tồn tại" });
     }
   });
 });
