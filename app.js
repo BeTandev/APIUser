a950b06fa6b36170cf022523d8152ef2c5371b26
DESKTOP-6RA1I0Q\nhutt <nhuttantranle@gmail.com>
2023-09-16 19:19:20 +0700

    edit app2

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 
-const userRouter = require("./users");
-const bookingRouter = require("./bookings");
-const messageRouter = require("./messages");
-
 const db = new sqlite3.Database("userData.sqlite3");
 
 db.serialize(() => {
@@ -50,10 +46,151 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/users", userRouter);
-app.use("/bookings", bookingRouter);
-app.use("/messages", messageRouter);
+app.get("/users", (req, res) => {
+  db.all("SELECT * FROM users", (err, rows) => {
+    if (err) {
+      console.error("Error getting users:", err);
+      res.status(500).json({ error: "Internal server error" });
+    } else {
+      res.json(rows);
+    }
+  });
+});
 
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.get("SELECT * FROM users WHERE id = ?", [id], (err, row) => {
+    if (err) {
+      console.error("Error getting user:", err);
+      res.status(500).json({ error: "Internal server error" });
+    } else if (row) {
+      res.json(row);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  });
+});
+
+app.post("/users", (req, res) => {
+  const { username, password, email } = req.body;
+
+  // Truy vấn cơ sở dữ liệu để kiểm tra tên người dùng đã tồn tại hay chưa
+  db.get("SELECT username FROM users WHERE username = ?", [username], (err, row) => {
+    if (err) {
+      console.error("Error checking username:", err);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+
+    if (row) {``
+      // Tên người dùng đã tồn tại, trả về phản hồi lỗi
+      res.status(400).json({ error: "Username already exists" });
+      return;
+    }
+
+    // Tên người dùng chưa tồn tại, thêm người dùng mới vào cơ sở dữ liệu
+    db.run(
+      "INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+      [username, password, email],
+      (err) => {
+        if (err) {
+          console.error("Error creating user:", err);
+          res.status(500).json({ error: "Internal server error" });
+        } else {
+          res.json({ message: "User created successfully" });
+        }
+      }
+    );
+  });
+});
+
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { username, password, email } = req.body;
+  db.run(
+    "UPDATE users SET username = ?, password = ?, email = ? WHERE id = ?",
+    [username, password, email, id],
+    (err) => {
+      if (err) {
+        console.error("Error updating user:", err);
+        res.status(500).json({ error: "Internal server error" });
+      } else {
+        res.json({ message: "User updated successfully" });
+      }
+    }
+  );
+});
+
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.run("DELETE FROM users WHERE id = ?", [id], (err) => {
+    if (err) {
+      console.error("Error deleting user:", err);
+      res.status(500).json({ error: "Internal server error" });
+    } else {
+      res.json({ message: "User deleted successfully" });
+    }
+  });
+});
+
+// API để đặt xe
+app.post("/bookings", (req, res) => {
+  const { name, phone, pickupLocation, destination, carType, note } = req.body;
+
+  db.run(
+    "INSERT INTO bookings (name, phone, pickupLocation, destination, carType, note) VALUES (?, ?, ?, ?, ?, ?)",
+    [name, phone, pickupLocation, destination, carType, note],
+    function (err) {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Lỗi khi tạo đơn đặt xe");
+      } else {
+        res.status(200).send("Đơn đặt xe đã được tạo thành công");
+      }
+    }
+  );
+});
+
+// Lấy danh sách các đơn đặt xe
+app.get("/bookings", (req, res) => {
+  db.all("SELECT * FROM bookings", function (err, rows) {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Lỗi khi lấy danh sách đơn đặt xe");
+    } else {
+      res.status(200).json(rows);
+    }
+  });
+});
+
+// GET: Lấy tất cả các tin nhắn
+app.get("/messages", (req, res) => {
+  db.all("SELECT * FROM messages", (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Lỗi server" });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+// POST: Tạo một tin nhắn mới
+app.post("/messages", (req, res) => {
+  const { name, email, phonenumber, message } = req.body;
+  const sql =
+    "INSERT INTO messages (name, email, phonenumber, message) VALUES (?, ?, ?, ?)";
+  const params = [name, email, phonenumber, message];
+
+  db.run(sql, params, function (err) {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Lỗi server" });
+    } else {
+      res.json({ id: this.lastID });
+    }
+  });
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
